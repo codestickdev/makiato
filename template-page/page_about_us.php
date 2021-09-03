de44b710a3df5f03b9f38be69c44d383393eca1b
Piotr <piotr.gawelda@microsoft.wsei.edu.pl>
2021-09-03 14:21:28 +0200

    about us page

--- template-page/page_about_us.php
+++ template-page/page_about_us.php
@@ -15,6 +15,119 @@
                 … a naszym uczuciem dzielimy się z Parami Młodymi, 
                 które pragną by ich przyjęcie weselne było wyjątkowe! </p>
         </div>
+        <div class="vice__master">
+            <div class="leftSide">
+                <h2>Vice-mistrz świata Coffee in Good Spirits. jedyny taki barista na wesele</h2>
+                <p>… a gdyby tak Wasi Goście mogli napić się kawy przyrządzonej przez wielokrotnego mistrza Polski i vicemistrza świata w prestiżowym konkursie Coffee in Good Spirits? 
+                    Oprócz doskonałego smaku serwuję Gościom pokaz niezwykłych umiejętności, dobry humor oraz mistrzowskie wskazówki!
+                </p>
+            </div>
+            <div class="rightSide">
+
+            </div>
+        </div>
+        <div class="master-cv">
+            <div class="leftSide">
+                <h2>mistrzowskie cv</h2>
+                <ul>
+                    <li>Mistrz Polski Brewers Cup 2014</li>
+                    <li>Vice Mistrz Swiata Coffee in Good Spirits, 2013</li>
+                    <li>Mistrz Polski Coffee in Good Spirits, 2013</li>
+                    <li>Vice Mistrz Polski Aeropress, 2013</li>
+                    <li>II Vice Mistrz Polski Cup Tasters, 2013</li>
+                    <li>Mistrz Polski Brewers Cup, 2012</li>
+                </ul>
+            </div>
+            <div class="rightSide">
+                <h2>mistrzowskie cv</h2>
+                <ul>
+                    <li>Mistrz Polski Brewers Cup 2014</li>
+                    <li>Vice Mistrz Swiata Coffee in Good Spirits, 2013</li>
+                    <li>Mistrz Polski Coffee in Good Spirits, 2013</li>
+                    <li>Vice Mistrz Polski Aeropress, 2013</li>
+                    <li>II Vice Mistrz Polski Cup Tasters, 2013</li>
+                    <li>Mistrz Polski Brewers Cup, 2012</li>
+                </ul>
+            </div>
+        </div>
+        <div class="inspiration">
+            <h1>czas na łyk inspiracji</h1>
+            <p>Wokół kawy kręci się moje życie, dlatego dbam o to, żeby każda filiżanka kawy na przyjęciu weselnym była perfekcyjnie zaserwowana!</p>
+        </div>
+        <div class="aboutUs__info">
+            <div class="box">
+                <div class="title">
+                    <p>mistrzostwo</p>
+                </div>
+                <div class="description">
+                    <p>Nasz główny barista to Wicemistrz Świata!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>zaangażowanie</p>
+                </div>
+                <div class="description">
+                    <p>Każde wesele jest dla nas najważniejsze!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>kompleksowość</p>
+                </div>
+                <div class="description">
+                    <p>Przejmujemy wszystkie obowiązki!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>wybór stylu</p>
+                </div>
+                <div class="description">
+                    <p>Dobierz bar i dress code baristów do klimatu swojego wesela! </p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>jakość / wyborny smak</p>
+                </div>
+                <div class="description">
+                    <p>Pracujemy z wysokiej jakości ziarnami oraz sprzętem!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>eko</p>
+                </div>
+                <div class="description">
+                    <p>Dbamy o planetę!</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>w całej Polsce</p>
+                </div>
+                <div class="description">
+                    <p>Nie ma dla nas zakamarków do których nie dotrzemy	</p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>elastyczność</p>
+                </div>
+                <div class="description">
+                    <p>Jesteśmy otwarci na wszelkie życzenia Par Młodych! </p>
+                </div>
+            </div>
+            <div class="box">
+                <div class="title">
+                    <p>piękno</p>
+                </div>
+                <div class="description">
+                    <p>Weselna kawa nie może obyć wzorków z mleka!</p>
+                </div>
+            </div>
+        </div>
     </div>
 </section>
 
